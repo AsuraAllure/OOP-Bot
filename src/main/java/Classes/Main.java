7094348ce08f49a5cc99edef501452f6f6deac09
kastryulya <ukorsunova02@gmail.com>
2022-12-02 12:55:35 +0500

    Added test scripts

--- src/main/java/Classes/Main.java
+++ src/main/java/Classes/Main.java
@@ -1,30 +1,27 @@
 package Classes;
 
-
-import Console.ConsoleBot;
-import Console.ConsoleFactory;
-//import Telegram.TelegramBot;
-//import Telegram.TelegramFactory;
-//import org.telegram.telegrambots.meta.TelegramBotsApi;
-//import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-//import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import Telegram.TelegramBot;
+import Telegram.TelegramFactory;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class Main {
 	public static void main(String[] args) {
-        ConsoleBot b1 = new ConsoleBot(new ConsoleFactory());
-        b1.run();
-
-//        try {
-//            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
-//            telegramBotsApi.registerBot(new TelegramBot(new TelegramFactory()));
-//        } catch (
-//                TelegramApiException e) {
-//            e.printStackTrace();
-//        }
+        try {
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot(new TelegramBot(new TelegramFactory()));
+        } catch (
+                TelegramApiException e) {
+            e.printStackTrace();
+        }
 
 
 		// Вариант метода main(String[] args) для консольной версии бота
         /*
+        import Console.ConsoleBot;
+        import Console.ConsoleFactory;
+
         ConsoleBot b1 = new ConsoleBot(new ConsoleFactory());
         b1.run();
         */
